560c5811a34d258e91530aa299ee0a2385d933c3
tiendat09b-afk <tiendat09b@gmail.com>
2026-03-18 21:10:39 +0700

    Create admin.py

---
+++ EmotionDetection/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Question, Choice, Submission
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
