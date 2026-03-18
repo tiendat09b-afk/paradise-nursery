605c626b0172f00cfdba3e183d895e347b17154b
tiendat09b-afk <tiendat09b@gmail.com>
2026-03-18 21:25:47 +0700

    Update admin.py

--- EmotionDetection/admin.py
+++ EmotionDetection/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Question, Choice, Submission
+
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 
 class ChoiceInline(admin.TabularInline):
@@ -20,6 +21,10 @@ class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
 
 
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
