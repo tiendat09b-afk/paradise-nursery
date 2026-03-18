80859f97dca9b5a38ed62d7e309f9a6aabd5e87d
tiendat09b-afk <tiendat09b@gmail.com>
2026-03-18 21:24:54 +0700

    Update models.py

--- EmotionDetection/models.py
+++ EmotionDetection/models.py
@@ -1,16 +1,32 @@
-class Question:
-    def __init__(self, text):
-        self.text = text
+from django.db import models
 
 
-class Choice:
-    def __init__(self, question, text):
-        self.question = question
-        self.text = text
+class Course(models.Model):
+    name = models.CharField(max_length=200)
 
 
-class Submission:
-    def __init__(self, user, question, choice):
-        self.user = user
-        self.question = question
-        self.choice = choice
+class Enrollment(models.Model):
+    user = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    grade = models.IntegerField()
+
+    def is_get_score(self, selected_ids):
+        correct_choices = self.choice_set.filter(is_correct=True)
+        correct_ids = set(c.id for c in correct_choices)
+        return correct_ids == set(selected_ids)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
